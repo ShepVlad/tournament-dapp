6bbd1c4de1ce0fab0c344e57e6a0966d19fc5873
ShepVlad <shepeliuk.link@gmail.com>
2025-07-02 07:02:34 +0300

    feat: show contract balance (prize pool) in tournament UI

--- src/web3/contract.ts
+++ src/web3/contract.ts
@@ -1,7 +1,7 @@
 import { ethers, Contract } from 'ethers';
 import abi from './abi.json';
 
-const CONTRACT_ADDRESS = '0xCe241ef0334A2C8eD7749A84Fca95335570b57B4';
+const CONTRACT_ADDRESS = '0xbc3B8c1eb1715bEb20f953a11D455C1019966D31';
 
 export const getContract = (
   runner: ethers.Signer | ethers.AbstractProvider
